2ecdf0a8b56ecea307dfae01840a806069a185f1
Jesse Palmer <jesselpalmer@gmail.com>
2023-11-11 11:08:32 -0800

    updates

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+import dns from 'dns';
+import util from 'util';
+
+const resolveMx = util.promisify(dns.resolveMx);
+
+async function checkMxRecords(email) {
+  const domain = email.split('@')[1];
+  try {
+    const records = await resolveMx(domain);
+    return records && records.length > 0;
+  } catch (error) {
+    return false;
+  }
+}
+
 const doesContainAtLeastOnePeriod = str => str.includes('.')
 const doesContainAtSymbol = str => str.includes('@')
 
